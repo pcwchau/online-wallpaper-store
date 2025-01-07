f7edcc114fad8233e7a6a0235d262995807d00e4
Peter <pccw2384@gmail.com>
2025-01-06 21:54:02 -0500

    add page in app router

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/product">Product</Link>
+        <Link href="/project">Project</Link>
+        <h1>Hello Next.js!</h1>
         {children}
       </body>
     </html>
--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>home page</h1>;
+  return <h1>product page</h1>;
 }
