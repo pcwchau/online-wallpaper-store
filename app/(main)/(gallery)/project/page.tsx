ad26ee4d4d679a74f18b34254e3ac6d0938fe77a
Peter <pccw2384@gmail.com>
2025-05-06 16:41:19 -0400

    Project page
    - add enlarge animation (only >640px)

--- app/(main)/(gallery)/project/page.tsx
+++ app/(main)/(gallery)/project/page.tsx
@@ -1,27 +1,43 @@
+"use client";
 import GeneralPage from "@/components/page/generalPage";
 import { projectCategoryRoutes } from "@/data/route";
+import useMediaQuery from "@/hook/useMediaQuery";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
+  const isLargeScreen = useMediaQuery("(min-width: 640px)");
+
   return (
     <GeneralPage title="Project">
-      <div className="grid grid-cols-2 gap-x-8 gap-y-6 lg:grid-cols-4 lg:gap-x-4">
+      <div className="grid grid-cols-2 gap-4">
         {projectCategoryRoutes.map((item, index) => (
-          <div key={index} className="space-y-2">
-            <div className="aspect-square relative">
-              <Link href={item.redirectUrl}>
+          <Link key={index} href={item.redirectUrl}>
+            <div className="relative group">
+              <div className="aspect-square relative overflow-hidden">
                 <Image
                   src={item.imageUrl}
                   alt={item.name}
                   fill
                   sizes="(max-width: 1024px) 50vw, 25vw"
-                  className="object-cover"
+                  className="object-cover transition-transform duration-700 ease-out group-hover:scale-110"
                 />
-              </Link>
+              </div>
+              {isLargeScreen ? (
+                <div
+                  className="absolute bottom-[10%] left-1/2 -translate-x-1/2 px-8 py-2 
+                  text-center font-bold text-sm bg-primary rounded-md 
+                hover:bg-secondary hover:text-secondary-text"
+                >
+                  {item.name}
+                </div>
+              ) : (
+                <div className="text-center font-bold text-sm py-2">
+                  {item.name}
+                </div>
+              )}
             </div>
-            <div className="text-center">{item.name}</div>
-          </div>
+          </Link>
         ))}
       </div>
     </GeneralPage>
--- components/page/generalPage.tsx
+++ components/page/generalPage.tsx
@@ -5,7 +5,7 @@ interface GeneralPageProps {
 
 export default function GeneralPage(props: GeneralPageProps) {
   return (
-    <div className="space-y-12 w-5/6 lg:w-2/3 mx-auto">
+    <div className="space-y-12 w-5/6 lg:w-3/4 mx-auto">
       <div className="text-center text-3xl font-semibold">{props.title}</div>
       {props.children}
     </div>
