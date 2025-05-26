03c02d5466a57ead7f9cb78fdc0796455d470923
Peter <pccw2384@gmail.com>
2025-05-26 11:27:15 -0400

    Add TwoByTwoImageSection

--- app/(main)/product/custom-printing/page.tsx
+++ app/(main)/product/custom-printing/page.tsx
@@ -1,16 +1,18 @@
-import ImagePage2x2 from "@/components/page/imagePage2x2";
+import TwoByTwoImageSection from "@/components/image/twoByTwoImageSection";
+import GeneralPage from "@/components/page/generalPage";
 import { customPrintingProductCollections } from "@/data/product";
 import { ProductCategoryType } from "@/types/product";
 
 export default function Page() {
   return (
-    <ImagePage2x2
-      title={ProductCategoryType.CustomPrintingWallFabric}
-      images={customPrintingProductCollections.map((col) => ({
-        name: col.name,
-        imageUrl: col.imageUrl,
-        redirectUrl: col.productPageUrl,
-      }))}
-    />
+    <GeneralPage title={ProductCategoryType.CustomPrintingWallFabric}>
+      <TwoByTwoImageSection
+        images={customPrintingProductCollections.map((col) => ({
+          name: col.name,
+          imageUrl: col.imageUrl,
+          redirectUrl: col.productPageUrl,
+        }))}
+      />
+    </GeneralPage>
   );
 }
